14b164cf07a5696d51b29d6467fe3d08b03b87a8
learnerrayyy <kr.wang2025@gmail.com>
2025-11-30 17:16:09 +0000

    Add function prototypes to mydefs.h
    Added function prototypes for bit manipulation and block management.

--- mydefs.h
+++ mydefs.h
@@ -1,3 +1,25 @@
 #pragma once
+#include "csa.h"
 
 // Prototypes for other "private" functions etc.
+
+int popcount_mask(mask_t msk);
+//count how many bits are set to 1 in the mask
+//m : a 64-bits mask_t where each bit repersent an idx
+
+int block_index (csa* c, unsigned int offset);
+//Searches the CSA for a block whose offset equals offset.
+//return -1 if no block with the given offset exists
+
+int value_index (mask_t msk , int bit);
+//given a block's mask m and bit postion bit
+//return how many bot are set after this bit
+//this gives the index in the vals[] arrays where the value
+//corresponding to bit and should be found or inserted
+
+block* insert_block(csa* c, unsigned int offset);
+//to insert a new block by the offset given to csa
+//block sorted by offset
+//to initialized new block: msk = 0; vals = null; 
+
+
